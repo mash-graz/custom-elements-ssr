25e7c0d87f886d9c69e8a08ee326f7c9ba337d99
Martin Schitter <ms+git@mur.at>
2023-10-23 04:55:53 +0200

    typedoc documentation generation

--- src/dsd-polyfill.ts
+++ src/dsd-polyfill.ts
@@ -1,4 +1,25 @@
-function process_dsd(root: HTMLElement | ShadowRoot) {
+/**
+ * Polyfill for Declarative Shadow DOM handling
+ * 
+ * This module will check the DSD support of the utilized browser
+ * and install polyfill handlers for additional processing if needed.
+ * 
+ * @remark You may utilize the `"module"` attribute of HTML 
+ * `<script …>`-tags to prevent multiply loading of this 
+ * polyfill if it appears more than once in the page source.
+ * 
+ * @module
+ */
+
+/**
+ * Search for all DSD templates and replace them by shadow DOM nodes
+ * in a recursive manner.
+ * 
+ * @param root Root node of search and replacement process 
+ *  
+ * @see [Polyfill](https://developer.chrome.com/articles/declarative-shadow-dom/#polyfill)
+ */
+export function process_dsd(root: HTMLElement | ShadowRoot) {
 	// console.log("DSR polyfill processing...");
 	root.querySelectorAll("template[shadowrootmode]")
 		.forEach(template => {
@@ -10,7 +31,15 @@ function process_dsd(root: HTMLElement | ShadowRoot) {
 		});
 }
 
-function install_polyfill_handler() {
+/**
+ * 
+ * Installs an event handler to swap all DSD templates for 
+ * shadow DOM nodes as soon as the page was loaded.
+ *
+ * @see [Polyfill](https://developer.chrome.com/articles/declarative-shadow-dom/#polyfill)
+ * 
+ */
+export function install_polyfill_handler() {
 	// console.log("install DSR polyfill handler...")
 	if (document.readyState === "loading") {
 		window.addEventListener('DOMContentLoaded', () => process_dsd(document.body), {
@@ -22,7 +51,21 @@ function install_polyfill_handler() {
 	}
 }
 
+/**
+ * Test if this particular browser supports Declarative Shadow DOM
+ * 
+ * @returns 
+ * - `true`: Declarative shadow DOM support is provided 
+ *   out of the box.
+ * - `false`: Customized handling of declarative shadow DOM 
+ *   is needed.
+ * 
+ * @see [Feature detection and browser support](https://developer.chrome.com/articles/declarative-shadow-dom/#feature-detection-and-browser-support)
+ */
+export function dsd_available(): boolean {
+	return HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode');
+}
 
-if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')) {
+if (!dsd_available()) {
 	install_polyfill_handler();
 }
